f53900891aa32a69d6719fee0026c7cdc08efd8f
PedroSantiago97 <pedro.santiago8051@outlook.com>
2023-06-14 08:32:33 -0300

    att

---
+++ js-ex03/exe03.js
@@ -0,0 +1,8 @@
+var metpay = 'Dinheiro'
+console.log(`Passa no ${metpay}, Senhor?`)
+if (metpay == 'Credito' ){
+    console.log('Como é amigo? Ta achando que eu to liso?')
+}
+else{
+    console.log('Passe logo, e desapareça de minha frente que eu nao gosto de pobre!')
+}
